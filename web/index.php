efe72479fe70e9309a61896a9e6bf55bf799262f
John Doe <johndoe@example.com>
2018-04-15 22:06:03 +0800

    博客停更,留存数据和代码

---
+++ web/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangqingbai
+ * Date: 2017-07-29 0029
+ * Time: 17:14
+ */
+header("location:http://guofu.com/home/Controller/index_controller.php?pagenow=1");
